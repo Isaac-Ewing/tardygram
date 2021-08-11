fec04178707e433285f621c95f0803ad5260a645
Missael Ortiz <ortiz.missael80@gmail.com>
2021-08-11 11:19:23 -0700

    added working create post route, tests green!

--- __tests__/post.test.js
+++ __tests__/post.test.js
@@ -2,6 +2,8 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const User = require('../lib/models/User');
+const Post = require('../lib/models/Post');
 
 jest.mock('../lib/middleware/ensure-auth.js', () => (req, res, next) => {
   req.user = {
@@ -17,10 +19,12 @@ describe('tardygram routes', () => {
   });
 
   it('creates a post via POST', async () => {
-    const postInfo = { photo_url: 'http://example.com/photo.jpg', caption: 'Hahaha, so #relatable', tags: ['relatable', 'cool', 'influencer'], username: 'test_user' };
+    const user = await User.insert({ github_login: 'test_user', github_avatar_url: 'http://example.com/image.png' });
+    const postInfo = { photo_url: 'http://example.com/photo.jpg', caption: 'Hahaha, so #relatable', tags: ['relatable', 'cool', 'influencer'] };
 
     const res = await request(app).post('/api/v1/posts').send(postInfo);
 
-    expect(res.body).toEqual({ id: '1', ...postInfo });
+    expect(res.body).toEqual({ id: '1', ...postInfo, username: user.github_login });
   });
+
 });
