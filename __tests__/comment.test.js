a702cd51774f37a429269486b6477eaa205cf7ca
BenFrancka <benfrancka@gmail.com>
2021-08-11 15:35:41 -0700

    adds post route to comments controller, test is now passing

--- __tests__/comment.test.js
+++ __tests__/comment.test.js
@@ -22,14 +22,18 @@ describe('tardygram routes', () => {
     const user = await User.insert({ github_login: 'test_user', github_avatar_url: 'http://example.com/image.png' });
     const post = await Post.insert({ photo_url: 'http://example.com/photo.jpg', caption: 'Hahaha, so #relatable', tags: ['relatable', 'cool', 'influencer'] });
     const comment = {
-      comment_by: user.github_login,
       post: post.id,
       comment: 'You take pictures good'
     };
 
     const res = await request(app).post('/api/v1/comments').send(comment);
 
-    expect(res.body).toEqual({ id: '1', ...comment });
+    expect(res.body).toEqual({ 
+      id: '1', 
+      post: 1, 
+      comment: 'You take pictures good',  
+      comment_by: user.github_login 
+    });
   });
 
 });
