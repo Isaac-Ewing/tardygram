c8722e69fed9996f7004df52ecb3849599019e7b
Sofia Tejada <sofianais@icloud.com>
2021-08-12 10:05:58 -0700

    test fails

---
+++ __tests__/user.test.js
@@ -0,0 +1,59 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+const User = require('../lib/models/User');
+const Comment = require('../lib/models/Comment');
+
+jest.mock('../lib/middleware/ensure-auth.js', () => (req, res, next) => {
+  req.user = {
+    username: 'test_user',
+    avatar_url: 'http://example.com/image.png'
+  };
+  next();
+});
+
+describe('bonus routes', () =>{
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('10 most popular users', async () => {
+    for(let i = 0; i < 10; i++) {
+      const user = await User.insert({ github_login: `user${i + 1}`, github_avatar_url: 'http://example.com/image.png' });
+      const postName = `post${i + 1}`;
+      await Post.insert({ photo_url: 'http://example.com/photo3.jpg', caption: `haha, this is post ${postName}`, tags: ['relatable', 'cool', 'influencer'], username: user.github_login });
+    }
+
+    for(let i = 0; i < 10; i++) {
+      const postIndex = i + 1;
+      await Comment.insert({ 
+        comment: 'groovy',
+        comment_by: `user${postIndex}`,
+        post: postIndex
+      });
+    }
+
+    for(let i = 0; i < 5; i++) {
+      const postIndex = i + 1;
+      await Comment.insert({ 
+        comment: 'groovy',
+        comment_by: `user${postIndex}`,
+        post: postIndex
+      });
+    }
+
+    for(let i = 0; i < 2; i++) {
+      const postIndex = i + 1;
+      await Comment.insert({ 
+        comment: 'groovy',
+        comment_by: `user${postIndex}`,
+        post: postIndex
+      });
+    }
+
+    const res = await request(app).get('/api/v1/users/popular');
+
+    expect(res.body.length).toEqual(10);
+  });
+});
