5607f8f763cdc85822cba92fa14f4a2e67a0b002
Isaac <isaac.ewing3@gmail.com>
2021-08-12 14:29:55 -0600

    failing test for 2nd bonus

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -57,4 +57,31 @@ describe('bonus routes', () => {
 
     expect(res.body.length).toEqual(10);
   });
+
+  it('10 highest posting users', async () => {
+    for(let i = 0; i < 11; i++) {
+      const user = await User.insert({ github_login: `user${i + 1}`, github_avatar_url: 'http://example.com/image.png' });
+      const postName = `post${i + 1}`;
+      await Post.insert({ photo_url: 'http://example.com/photo3.jpg', caption: `haha, this is post ${postName}`, tags: ['relatable', 'cool', 'influencer'], username: user.github_login });
+    }
+
+    for(let i = 0; i < 10; i++) {
+      const postName = i + 1;
+      await Post.insert({ photo_url: 'http://example.com/photo3.jpg', caption: `haha, this is post ${postName}`, tags: ['relatable', 'cool', 'influencer'], username: `user${i + 1}` });
+    }
+
+    for(let i = 0; i < 5; i++) {
+      const postName = i + 1;
+      await Post.insert({ photo_url: 'http://example.com/photo3.jpg', caption: `haha, this is post ${postName}`, tags: ['relatable', 'cool', 'influencer'], username: `user${i + 1}` });
+    }
+
+    for(let i = 0; i < 2; i++) {
+      const postName = i + 1;
+      await Post.insert({ photo_url: 'http://example.com/photo3.jpg', caption: `haha, this is post ${postName}`, tags: ['relatable', 'cool', 'influencer'], username: `user${i + 1}` });
+    }
+
+    const res = await request(app).get('/api/v1/users/prolific');
+
+    expect(res.body.length).toEqual(10);
+  });
 });
